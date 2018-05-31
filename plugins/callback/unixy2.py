dfb17ebad5234e3a4c77056dab98818881727239
ivandeex <ivandeex@gmail.com>
2018-05-31 15:48:48 +0000

    inventory, playbooks, plugins, scripts
    starter: scripts, roles, starter playbook
    
    new scripts:
    - role: role runner
    - lin-refresh: wait for user before reboot
    - activate: activate ansible virtual environment
    - play: ansible-playbook wrapper
    - check git status of roles (git-roles)
    - show-graph: print usage
    - import-roles
    - run-role
    - make-role: script and skeleton
    - vultr: role and wrapper script
    
    roles:
    - lin_system
    - lin_enter
    - lin_hosters
    - lin_docker
    - dev_user
    - dev_jupyter
    - dev_ivantory
    - dev_user
    
    vagrant boxes:
    - enter-vagrant box disables telemetry and removes apport
    - enable vagrant-share for all vagrant machines
    - dev_vagrant
    - vag_share
    
    playbooks:
    - starter
    - ivantory
    - master
    - bastion
    - devops
    - keenetic (experimental playbook)
    
    certbot/letencrypt roles:
    - cloudflare
    - push master/replica
    - letsencrypt_copy
    - separate letsencrypt certificate for sip tests
    
    nginx roles:
    - lin-nginx
    - lin-uwsgi
    - nginx_base, db_base: common nginx/web settings
    
    inventory:
    - prefer underscore in role names and skel
    - group workspace -> permitted
    - organize group entries by subdirectory
    - prepend numbers to group entries to fix load ordering errors
    - settings/checkfile for cryptor
    
    role backup_base
    
    asterisk roles:
    - base
    - core
    - soho
    - providers
    - billing
    
    database / server roles:
    - srv_cgpro
    - srv_mongodb
    - srv_redis_ssl
    
    mark tasks without ipv6 with tag ip4only
    
    devops / hosting roles:
    - dev_hosters (+awless, packet cli)
    - dev_terraform (+ vultr/ansible plugins)
    - dev_packer
    - dev_vagrant
    - dev_ivantory
    
    local roles:
    - lin_ipv6_cloudflare
    - upgrade vag1 to bionic
    - vag_vbguest (setup vb guest additions on vagrant)
    - vag_share
    - vag_ipv6dns
    
    bastion roles:
    - lin_shadowsocks
    - v2ray support
    - srv_chisel
    - srv_doh
    - srv_squid
    - lin_dante_auth
    - lin_mtproxy
    - lin_tor
    - lin_ziproxy
    
    config:
    - set fact cache timeout to 10m
    - callback: unixy2 (less output, mark ignored failures)
    - logging callback
    - temporarily silence deprecation warnings
    
    new roles:
    - lin_rclone

---
+++ plugins/callback/unixy2.py
@@ -0,0 +1,105 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from datetime import datetime
+from ansible import constants as C
+from ansible.module_utils._text import to_text
+from ansible.plugins.callback.unixy import CallbackModule as CallbackBase
+
+
+class CallbackModule(CallbackBase):
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'stdout'
+    CALLBACK_NAME = 'unixy2'
+    CALLBACK_NEEDS_WHITELIST = False
+
+    def __init__(self):
+        super(CallbackModule, self).__init__()
+        self._last_role_name = ''
+
+    def _get_task_display_name(self, task):
+        self.task_display_name = None
+        display_names = task.get_name().strip().split(" : ")
+        role_name = display_names[-2] if len(display_names) > 1 else ''
+        task_name = display_names[-1]
+        if task_name.startswith("include"):
+            return
+        if role_name != self._last_role_name:
+            self._last_role_name = role_name
+            if role_name:
+                task_name = '%s \u2014 %s' % (task_name, role_name)
+        self.task_display_name = task_name
+
+    def _run_is_verbose(self, result, verbosity=0):
+        return ((self._display.verbosity > verbosity or result._result.get('_ansible_verbose_always', False) is True)
+                and result._result.get('_ansible_verbose_override', False) is False)
+
+    def _process_result_output(self, result, msg):
+        task_host = result._host.get_name()
+        task_result = "%s %s" % (task_host, msg)
+
+        task_action = result._task.action
+        result_msg = result._result.get('msg', '')
+        if task_action in ('debug', 'assert') and msg == "ok" and result_msg:
+            try:
+                debug_msg = self._dump_results(result_msg, indent=4)
+            except:
+                debug_msg = to_text(result_msg)
+            return "%s %s | %s: %s" % (task_host, msg, task_action, debug_msg)
+
+        if self._run_is_verbose(result, verbosity=1):
+            return "%s %s: %s" % (task_host, msg, self._dump_results(result._result, indent=4))
+
+        if self.delegated_vars:
+            task_delegate_host = self.delegated_vars['ansible_host']
+            task_result = "%s -> %s %s" % (task_host, task_delegate_host, msg)
+
+        if result_msg and result_msg != "All items completed":
+            task_result += " | msg: " + to_text(result_msg)
+
+        if result._result.get('stdout') and self._run_is_verbose(result, verbosity=0):
+            task_result += " | stdout: " + result._result.get('stdout')
+
+        if result._result.get('stderr'):
+            task_result += " | stderr: " + result._result.get('stderr')
+
+        return task_result
+
+    def v2_playbook_on_play_start(self, play):
+        name = play.get_name().strip()
+        if name and play.hosts:  # replace hyphens by endash
+            msg = u"\n\u2013 %s on hosts: %s \u2013" % (name, ",".join(play.hosts))
+        else:
+            msg = u"---"
+        self._display.display(msg)
+
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        self._get_task_display_name(task)
+        if self.task_display_name is not None:
+            ts = datetime.now().strftime("%H:%M:%S")
+            self._display.display("%s\u2026 %s" % (ts, self.task_display_name))
+
+    def v2_playbook_on_handler_task_start(self, task):
+        self._get_task_display_name(task)
+        if self.task_display_name is not None:
+            ts = datetime.now().strftime("%H:%M:%S")
+            self._display.display("%s\u2026 %s (via handler)... " % (ts, self.task_display_name))
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        self._preprocess_result(result)
+        if ignore_errors:
+            msg = "failed (ignored)"
+            display_color = C.COLOR_WARN
+        else:
+            display_color = C.COLOR_ERROR
+            msg = "failed"
+        task_result = self._process_result_output(result, msg)
+        self._display.display("  " + task_result, display_color)
+
+    def v2_runner_on_unreachable(self, result):
+        self._preprocess_result(result)  # fixes bug in ansible 2.8.1
+        task_result = self._process_result_output(result, "unreachable")
+        self._display.display("  " + task_result, C.COLOR_UNREACHABLE)
+
+    def v2_runner_on_start(self, host, task):
+        pass  # silence warning in ansible 2.9
